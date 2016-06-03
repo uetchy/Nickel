97dd1e38be899d6b16559e590fd2e3364685ee01
Yasuaki Uechi <uetchy@randompaper.co>
2016-06-03 21:38:21 +0900

    Add basic controls

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,16 +1,50 @@
-var {remote} = require('electron');
-var win = remote.getCurrentWindow();
-var player = document.getElementById('player');
+const {remote} = require('electron');
+const win = remote.getCurrentWindow();
+const player = document.getElementById('player');
+const playButton = document.getElementById('play');
+const playback = document.getElementById('playback');
+const volumeBar = document.getElementById('volume-bar');
+
+var isSeeking = false;
 
 player.addEventListener('loadedmetadata', function() {
-  const {videoWidth, videoHeight} = this;
+  const {videoWidth, videoHeight, duration, initialTime} = this;
   win.setSize(videoWidth, videoHeight);
   win.setAspectRatio(videoWidth/videoHeight);
+
+  playback.min = playback.value = initialTime || 0;
+  playback.max = duration;
+
+  playButton.disabled = false;
   this.play();
 });
 
-player.addEventListener('seeked', function(e) {
-  console.log(e);
+player.addEventListener("timeupdate", function() {
+  if (!isSeeking) playback.value = player.currentTime;
+});
+
+playButton.addEventListener('click', function() {
+  if (player.paused) {
+    player.play();
+  } else {
+    player.pause();
+  }
+});
+
+playback.addEventListener("mousedown", function() {
+  isSeeking = true;
+});
+
+playback.addEventListener("mouseup", function(e) {
+  isSeeking = false;
+});
+
+playback.addEventListener('change', function() {
+  player.currentTime = this.valueAsNumber;
 })
 
-player.src = '../test.mp4';
+volumeBar.addEventListener("change", function() {
+  player.volume = volumeBar.valueAsNumber;
+});
+
+player.src = '../test2.mp4';
